de2fc74634466b11b9cfe218f511a089347a7b0b
PanRagon <thebigecko@gmail.com>
2020-01-30 13:33:06 +0100

    Create a basic Quiz game where the user can generate a random question with 4 answer alternatives

---
+++ public/code.js
@@ -0,0 +1,55 @@
+const quizzes = [
+    {
+        question: "Who created Javascript?",
+        answers: ["Brendan Eich", "Jacob Eich", "Brandon Lake", "James Gosling"],
+        correctAnswer: 0
+    },
+    {
+        question: "What company owns Github?",
+        answers: ["Apple", "Amazon", "Microsoft", "Git"],
+        correctAnswer: 2
+    },
+    {
+        question: "Who's Joe?",
+        answers: ["Joe Pesci", "Joe Mama!", "Joseph Stalin", "Joe Biden"],
+        correctAnswer: 1
+    }
+]
+
+let guessed = false;
+let correctAnswer;
+const questionDiv = document.getElementById("question");
+const answerAlternatives = document.getElementById("answer-alternatives");
+const answerDiv1 = document.getElementById("answer1");
+const answerDiv2 = document.getElementById("answer2");
+const answerDiv3 = document.getElementById("answer3");
+const answerDiv4 = document.getElementById("answer4");
+const result = document.getElementById("result");
+const newQuestionButton = document.getElementById("new-game-btn");
+
+function giveQuestion() {
+    result.innerText = "";
+    let quiz = quizzes[Math.floor(Math.random() * quizzes.length)];
+    correctAnswer = quiz.correctAnswer;
+    questionDiv.innerText = quiz.question;
+    let answersDivs = answerAlternatives.children
+    for(let i = 0; i < answersDivs.length; i++) {
+        answersDivs[i].innerText = quiz.answers[i];
+        answersDivs[i].onclick = () => verifyAnswer(i);
+    }
+    guessed = false;
+}
+
+function verifyAnswer(answer) {
+    if(guessed === false) {
+        if(answer === correctAnswer) {
+            result.innerText = "That's correct, good job!"
+        } else {
+            result.innerText = "Sorry, you guessed wrong, try a new question!"
+        }
+        guessed = true;
+    }
+}
+
+newQuestionButton.onclick = () => giveQuestion();
+giveQuestion();
